8eb6017632e696f8ae784ea23bd4b73ba1ffb49c
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-07-14 09:26:31 -0400

    Fix import statement for Pastors component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Home from "./home";
 import NotFound from "./NotFound";
 import Churches from "./Churches";
-import Pastors from "./Pastors";
+import Pastors from "./pastors";
 export { Home, NotFound, Churches, Pastors };
