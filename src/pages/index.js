bbeebf8bcd30ce4ab67b06535e079068a17fb0e9
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-07-14 09:23:29 -0400

    Add Pastors to exports in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Home from "./home";
 import NotFound from "./NotFound";
 import Churches from "./Churches";
-export { Home, NotFound, Churches };
+import Pastors from "./Pastors";
+export { Home, NotFound, Churches, Pastors };
